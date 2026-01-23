8515a11292275742cf7808a074753cadef0369ee
Aya Ahmed <ayaa95925@gmail.com>
2026-01-23 14:39:57 +0200

    Add firebase to my progect .

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:evently_app/firebase_options.dart';
 import 'package:evently_app/l10n/app_localizations.dart';
 import 'package:evently_app/ui/screen/Login/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const EventlyApp());
 }
 
